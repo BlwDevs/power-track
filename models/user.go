19b5af35b6c5c5254a976d49c395db206e6d9997
Vithor Freitas <vithorfreitass@gmail.com>
2025-05-20 11:27:17 -0300

    add: role in user model

--- models/user.go
+++ models/user.go
@@ -4,14 +4,23 @@ import (
 	"gorm.io/gorm"
 )
 
-//Domínio para o atributo plano do usuário
+// Domínio para o atributo plano do usuário
 type Plan int
-//Variáveis numéricas para o plano do usuário
+
+// Variáveis numéricas para o plano do usuário
 const (
-	Free  Plan = 0
+	Free        Plan = 0
 	OneInverter Plan = 1
 	TwoInverter Plan = 2
 )
+
+type Role string
+
+const (
+	Admin  Role = "admin"
+	Common Role = "common"
+)
+
 type User struct {
 	gorm.Model
 	CPF     string `json:"cpf" gorm:"type:varchar(14);uniqueIndex"`
@@ -20,4 +29,5 @@ type User struct {
 	Address string `json:"address" gorm:"type:varchar(255)"`
 	Plan    Plan   `json:"plan" gorm:"type:int;default:0"`
 	Phone   string `json:"phone" gorm:"type:varchar(20);uniqueIndex"`
-} 
+	Role    Role   `json:"role" gorm:"type:varchar(10);default:'common'"`
+}
