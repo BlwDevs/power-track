020d52b909e35a5c8b1181a95c2e2ee4d9b20af4
Vithor Freitas <vithorfreitass@gmail.com>
2025-05-22 20:33:45 -0300

    upd: add inverter fk in userParserInverter and remove userParser from inverter model

--- models/inverter.go
+++ models/inverter.go
@@ -15,10 +15,8 @@ const (
 type Inverter struct {
 	gorm.Model
 
-	SerialNumber string             `json:"serial_number" gorm:"type:varchar(50)"`
-	Status       Status             `json:"status" gorm:"type:varchar(50)"`
-	Power        float64            `json:"power" gorm:"type:decimal(10,2)"`
-	TotalEnergy  float64            `json:"total_energy" gorm:"type:decimal(10,2)"`
-	UserParserID uint               `json:"user_parser_id" gorm:"not null"`
-	UserParser   UserParserInverter `gorm:"foreignKey:UserParserID"`
+	SerialNumber string  `json:"serial_number" gorm:"type:varchar(50)"`
+	Status       Status  `json:"status" gorm:"type:varchar(50)"`
+	Power        float64 `json:"power" gorm:"type:decimal(10,2)"`
+	TotalEnergy  float64 `json:"total_energy" gorm:"type:decimal(10,2)"`
 }
