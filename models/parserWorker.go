4119e04cdbb2c03e3d716242af7c00e1f865fc0b
Vithor Freitas <vithorfreitass@gmail.com>
2025-05-22 23:17:38 -0300

    del: unique constraint from apiKey in parser worker

--- models/parserWorker.go
+++ models/parserWorker.go
@@ -9,7 +9,10 @@ type ParserWorker struct {
 	gorm.Model
 
 	//Fabricante do inversor
-	Manufacturer string `json:"manufacturer" gorm:"type:varchar(100);not null"`
+	Manufacturer string `json:"manufacturer" gorm:"type:varchar(100);not null;not blank"`
 	// Chave da API para autenticação
-	ApiKey string `json:"api_key" gorm:"type:varchar(255);not null;uniqueIndex"`
+	ApiKey string `json:"api_key" gorm:"type:varchar(255);not null;not blank"`
+
+	//Ativo ou inativo
+	Active bool `json:"active" gorm:"type:boolean;default:true"`
 }
